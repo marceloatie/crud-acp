05e0e46b6988670744f8ca1b10422a4ba6c67fda
Marcelo Atie <marceloatie@gmail.com>
2019-06-03 01:56:20 -0400

    Rotas

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,36 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('bemVindo');
 });
+
+//Professor
+//Route::resource('professor', 'ProfessorController');
+Route::any('/professor', 'ProfessorController@index');
+Route::any('/professor/novo', 'ProfessorController@novo');
+Route::any('/professor/insert', 'ProfessorController@insert');
+Route::any('/professor/editar/{id}', 'ProfessorController@editar');
+Route::any('/professor/update/{id}', 'ProfessorController@update');
+Route::any('/professor/delete/{id}', 'ProfessorController@delete');
+
+//Curso
+//Route::resource('curso', 'CursoController');
+Route::any('/curso', 'CursoController@index');
+Route::any('/curso/novo', 'CursoController@novo');
+Route::any('/curso/insert', 'CursoController@insert');
+Route::any('/curso/editar/{id}', 'CursoController@editar');
+Route::any('/curso/update/{id}', 'CursoController@update');
+Route::any('/curso/delete/{id}', 'CursoController@delete');
+
+//Aluno
+//Route::resource('curso', 'CursoController');
+Route::any('/aluno', 'AlunoController@index');
+Route::any('/aluno/novo', 'AlunoController@novo');
+Route::any('/aluno/insert', 'AlunoController@insert');
+Route::any('/aluno/editar/{id}', 'AlunoController@editar');
+Route::any('/aluno/update/{id}', 'AlunoController@update');
+Route::any('/aluno/delete/{id}', 'AlunoController@delete');
+Route::any('/aluno/ajaxcep/{cep}', 'AlunoController@ajaxCep');
+
+//Relatorio
+Route::any('/relatorio', 'RelatorioController@index');
